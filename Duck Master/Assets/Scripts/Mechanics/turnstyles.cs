ee099d99dd337ceaa6a18741b02971bbb65753ca
Michael Zheng <michael.zheng@mymail.champlain.edu>
2019-03-28 16:06:30 -0400

    turnstyles work on

---
+++ Duck Master/Assets/Scripts/Mechanics/turnstyles.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turnstyles : MonoBehaviour
+{
+	// Start is called before the first frame update
+
+	[SerializeField] GameObject downCollider;
+	[SerializeField] GameObject upCollider;
+	[SerializeField] int min;
+	[SerializeField] int max;
+	[SerializeField] int targetvalue;
+
+	int output = 0;
+	int getOutput(){ return output; }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void updateInput(GameObject collider)
+	{
+		if (upCollider == collider)
+		{
+			output++;
+		}
+		else
+		{
+			output--;
+		}
+	}
+}
