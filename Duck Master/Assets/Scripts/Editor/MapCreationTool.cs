e1adf06ec808bc26da4316222de21a3662f694ea
Alex <alexanderwest0598@gmail.com>
2019-04-02 01:15:52 -0400

    moved code, it works, kind of

--- Duck Master/Assets/Scripts/Editor/MapCreationTool.cs
+++ Duck Master/Assets/Scripts/Editor/MapCreationTool.cs
@@ -123,60 +123,7 @@ public class MapCreationTool : EditorWindow
 
 			if (GUILayout.Button("Create Level"))
 			{
-				//List<List<DuckTile.TileType>> typeGrid, List< List<bool> > baitableGrid, List<List<bool>> heightChangeGrid, int height
-				List<List<DuckTile.TileType>> typeGrid = new List<List<DuckTile.TileType>>();
-				List<DuckTile.TileType> typeList;
-				List<List<bool>> baitableGrid = new List<List<bool>>();
-				List<bool> baitableList;
-				List<List<bool>> heightChangeGrid = new List<List<bool>>();
-				List<bool> heightChangeList;
-				List<DuckTileGrid> tileGrids = new List<DuckTileGrid>();
-
-				for(int i = 0; i < verticalLevels; ++i)
-				{
-					// current height is i
-					for(int j = 0; j < height; ++j)
-					{
-						typeList = new List<DuckTile.TileType>();
-						baitableList = new List<bool>();
-						heightChangeList = new List<bool>();
-						for(int k = 0; k < width; ++k)
-						{
-							int index = height * j + k;
-							if(index < height * width)
-							{
-								string currentBlock = listGridSelStrings[i][index];
-								//if(currentBlock == null)
-								//{
-								//	Debug.Log("Null");
-								//}
-								// string[] blockTypes = { "Ground", "Water", "Damp", "None" };
-								if (currentBlock == blockTypes[0] || currentBlock == blockTypes[2])
-								{
-									// passable both
-									typeList.Add(DuckTile.TileType.PassableBoth);
-								}
-								else if(currentBlock == blockTypes[1])
-								{
-									// unpassable master
-									typeList.Add(DuckTile.TileType.UnpassableMaster);
-								}
-								else
-								{
-									// it's none/null aka null
-									typeList.Add(DuckTile.TileType.INVALID_TYPE);
-								}
-								baitableList.Add(false);
-								heightChangeList.Add(false);
-							}
-						}
-						typeGrid.Add(typeList);
-						baitableGrid.Add(baitableList);
-						heightChangeGrid.Add(heightChangeList);
-					}
-					tileGrids.Add(new DuckTileGrid(typeGrid, baitableGrid, heightChangeGrid, i));
-				}
-				MapGenerator.GenerateMap(tileGrids);
+				GameObject.Find("GameManager").GetComponent<MapGenerator>().GenerateMap(verticalLevels, listGridSelStrings, blockTypes, currentLevelHeights, currentLevelWidths);
 			}
 		}
 		// TO DO: Add the option to grab a ton of tiles with right click
