0d2224a43b3c85443de117f879fdadb6e033eb9b
Michael Zheng <michael.zheng@mymail.champlain.edu>
2019-04-01 21:55:46 -0400

    cog implemented

--- Duck Master/Assets/Scripts/Mechanics/CogCollider.cs
+++ Duck Master/Assets/Scripts/Mechanics/CogCollider.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class CogCollider : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "duck")
+		{
+			Vector3 colliderPosition = collision.gameObject.transform.position;
+			Vector3 objPosition = transform.position;
+			Vector3 originPosition = gameObject.transform.parent.position; 
+			float x = objPosition.x;
+			float z = objPosition.z;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+			float originX = originPosition.x;
+			float originZ = originPosition.z;
+
+			Vector2 cogDirection = new Vector2(z - originZ, -(x - originX));
+			Vector2 collisionDirection = new Vector2(colliderPosition.x - objPosition.x, colliderPosition.z - objPosition.z);
+
+			if(Vector2.Dot(cogDirection,collisionDirection) > 0)
+			{
+				gameObject.GetComponentInParent<cogs>().updateInput(1);
+			}
+			else
+			{
+				gameObject.GetComponentInParent<cogs>().updateInput(-1);
+			}
+		
+		}
+	}
+
 }
--- Duck Master/Assets/Scripts/Mechanics/cogs.cs
+++ Duck Master/Assets/Scripts/Mechanics/cogs.cs
@@ -6,6 +6,10 @@ public class cogs : MonoBehaviour
 {
     [SerializeField]int state = 0; 
     int getState() { return state; }
+
+	bool disableTurn = false;
+	float disableTimer = 1.5f;
+	float disableCounter = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +19,29 @@ public class cogs : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(disableTurn)
+		{
+			disableCounter += Time.deltaTime;
+			if(disableCounter > disableTimer)
+			{
+				disableTurn = false;
+			}
+		}
     }
+
+	//+ means clockwise, - means counterclockwise
+	public void updateInput(int rotateDirection)
+	{
+		if(disableTurn = false)
+		{
+			disableTurn = true;
+			disableCounter = 0;
+			state += rotateDirection;
+
+			transform.rotation = Quaternion.Euler(new Vector3(0, 90*state, 0));
+
+			//send input out
+		}
+
+	}
 }
