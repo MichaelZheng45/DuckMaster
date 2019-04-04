0693365fa320afda0041d1b03bb39d317b3d9514
Will Gordon <wgordon9999@gmail.com>
2019-04-04 15:05:03 -0400

    Created UIManager for management

---
+++ Duck Master/Assets/Scripts/Helpers/UIManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] GameObject pickUpButton;
+    [SerializeField] GameObject whistleButton;
+    [SerializeField] GameObject throwButton;
+    [SerializeField] double magnitude = 1.5;
+    bool throwToggle = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform duck = GameManager.Instance.getduckTrans();
+        Transform player = GameManager.Instance.getPlayerTrans();
+
+        //Determine whether to whistle or hold - Do the calculation here to avoid merge conflict
+        if ((duck.position - player.position).magnitude < magnitude && !GameManager.Instance.checkIsHoldingDuck())
+        {
+            whistleButton.SetActive(false);
+            pickUpButton.SetActive(true);
+        }
+
+        else
+        {
+            whistleButton.SetActive(true);
+            pickUpButton.SetActive(false);
+        }
+
+        //Check for throw
+        if (GameManager.Instance.checkIsHoldingDuck())
+            throwButton.SetActive(true);
+        else
+            throwButton.SetActive(false);
+        
+        if (throwToggle)
+        {
+            //if tap on tile throw
+        }
+        else
+        {
+            //path find call
+        }
+
+    }
+
+    public void ToggleThrow(bool toggle)
+    {
+        throwToggle = toggle;
+    }
+}
