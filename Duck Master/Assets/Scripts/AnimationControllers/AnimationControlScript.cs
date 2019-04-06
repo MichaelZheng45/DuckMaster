6b8e27dfb4d254fddf1d76623729024a70001fdb
Tom Lunday <thomas.lunday@mymail.champlain.edu>
2019-04-06 14:12:47 -0400

    Animation controller started

---
+++ Duck Master/Assets/Scripts/AnimationControllers/AnimationControlScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AnimationControlScript : MonoBehaviour
+{
+    Animator animator;
+
+    private void Start()
+    {
+        AnimationEventStuff.onWalkingChange += ChangeWalk;
+    }
+
+
+    void ChangeWalk(bool newWalk)
+    {
+        animator.SetBool("Walking", newWalk);
+    }
+
+
+}
---
+++ Duck Master/Assets/Scripts/AnimationControllers/AnimationEventStuff.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEventStuff : MonoBehaviour
+{
+    public delegate void CharacterEventHandler(bool newVal);
+
+    public static event CharacterEventHandler onWalkingChange;
+
+    public static void WalkingChange(bool newVal)
+    {
+        if (onWalkingChange != null)
+            onWalkingChange(newVal);
+    }
+
+
+}
