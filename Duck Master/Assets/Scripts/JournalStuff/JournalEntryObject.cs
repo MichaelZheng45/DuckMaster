970f0b89c8b5b00c625e7d83d552d679bb5af89f
Marc Bonilla <marcbb24@gmail.com>
2019-04-11 16:03:21 -0400

    Added journal ui and system to build

---
+++ Duck Master/Assets/Scripts/JournalStuff/JournalEntryObject.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewJournalEntry", menuName = "Journal/JournalEntry", order = 1)]
+public class JournalEntryObject : ScriptableObject
+{
+    public string JournalEntryName = "default";
+    public string JournalEntryText = "SampleText";
+}
