bb97275dc02a2df38b23488bfdf873dda3f441c7
Alex <alexanderwest0598@gmail.com>
2019-04-16 00:14:49 -0400

    fixed the clearing bug, none default, change unselects, change all, shortcuts for editing

--- Duck Master/Assets/Scripts/Editor/MapCreationTool.cs
+++ Duck Master/Assets/Scripts/Editor/MapCreationTool.cs
@@ -18,10 +18,12 @@ public class MapCreationTool : EditorWindow
 	int gridSel;
 	// 3d array to 1d array
 	// current index is (verticalLevel * widthOfLevel * heightOfLevel + yIndex * heightOfLevel + xIndex)
-	string[] listGridSelStrings;
+	//string[] listGridSelStrings;
+	List<List<List<string>>> listGridSelStrings;
 
 	string[] blockTypes = { "Ground", "Water", "None" };
-	int blockSelection;
+	int defaultBlockIndex = 2;
+	int blockSelectionIndex;
 
 	bool toggleSelector = false;
 
@@ -40,24 +42,45 @@ public class MapCreationTool : EditorWindow
      */
 	void OnGUI()
 	{
-		verticalLevels = EditorGUILayout.IntField("Max Number of Levels", verticalLevels);
+		GUILayout.Label("Layer Change / Selection");
+		verticalLevels = EditorGUILayout.IntField("Max Number of Layers", verticalLevels);
+		// make sure we have layers
 		if (verticalLevels != 0)
 		{
+			// setup new layers on number change
 			if (lastVerticalLevels != verticalLevels)
 			{
+				toggleSelector = false;
 				levelStringNums = new string[verticalLevels];
 
 				for (int i = 0; i < verticalLevels; i++)
 				{
 					levelStringNums[i] = "Level " + (i + 1);
 				}
+
+				// copy list of grids
+				List<List<List<string>>> oldList = listGridSelStrings;
+				listGridSelStrings = new List<List<List<string>>>();
+				for(int i = 0; i < verticalLevels; ++i)
+				{
+					if(oldList != null && oldList.Count > i)
+					{
+						listGridSelStrings.Add(oldList[i]);
+					}
+					else
+					{
+						listGridSelStrings.Add(new List<List<string>>());
+					}
+				}
 			}
 
+			// showing the level selection dropdown
 			if (levelStringNums != null)
 			{
-				levelSelection = EditorGUILayout.Popup("Level Selection", levelSelection, levelStringNums);
+				levelSelection = EditorGUILayout.Popup("Layer Selection", levelSelection, levelStringNums);
 			}
 
+			// if the widths are null create them, or if they're changed copy them
 			if (currentLevelWidths == null || verticalLevels != currentLevelWidths.Length)
 			{
 				currentLevelWidths = new int[verticalLevels];
@@ -67,6 +90,7 @@ public class MapCreationTool : EditorWindow
 				}
 				lastLevelWidths = new int[verticalLevels];
 			}
+			// if the height are null create them, or if they're changed copy them
 			if (currentLevelHeights == null || verticalLevels != currentLevelHeights.Length)
 			{
 				currentLevelHeights = new int[verticalLevels];
@@ -77,84 +101,121 @@ public class MapCreationTool : EditorWindow
 				lastLevelHeights = new int[verticalLevels];
 			}
 
-			blockSelection = EditorGUILayout.Popup("Block Selection", blockSelection, blockTypes);
+			GUILayout.Label("Block Selectors");
 
-			toggleSelector = EditorGUILayout.Toggle("Enable Map Changing", toggleSelector);
+			// select the initial block generated
+			defaultBlockIndex = EditorGUILayout.Popup("Initial Block Selection", defaultBlockIndex, blockTypes);
+
+			// selection for which block you're using
+			blockSelectionIndex = EditorGUILayout.Popup("Block Selection", blockSelectionIndex, blockTypes);
+
+			EditorGUILayout.PrefixLabel("Grid Changing");
+
+			// whether you want to change the grid or not
+			toggleSelector = EditorGUILayout.Toggle("Enable Grid Changing", toggleSelector);
+
+			if (GUILayout.Button("Change All") && listGridSelStrings.Count > 0)
+			{
+				ChangeAll(blockSelectionIndex);
+			}
+
+			GUILayout.Label("Grid Dimensions");
 
 			int height = 0, width = 0, lastHeight = 0, lastWidth = 0;
 
+			// make sure they're not null
 			if (currentLevelHeights != null && currentLevelWidths != null)
 			{
+				// make sure the level selection is valid, and that it's less than the number of grid dimensions
 				if (levelSelection >= 0 && levelSelection < currentLevelWidths.Length && levelSelection < currentLevelHeights.Length)
 				{
 					currentLevelWidths[levelSelection] = EditorGUILayout.IntField("Level Width", currentLevelWidths[levelSelection]);
 					currentLevelHeights[levelSelection] = EditorGUILayout.IntField("Level Height", currentLevelHeights[levelSelection]);
 				}
 
-				if (lastVerticalLevels != verticalLevels || lastLevelHeights[levelSelection] != currentLevelHeights[levelSelection] || lastLevelWidths[levelSelection] != currentLevelWidths[levelSelection] || lastLevelSelection != levelSelection)
-				{
-					int totalSize = 0, lastSize = 0;
-					if (currentLevelWidths != null && currentLevelHeights != null)
-					{
-						for (int i = 0; i < verticalLevels; ++i)
-						{
-							totalSize += currentLevelWidths[i] * currentLevelHeights[i];
-						}
-					}
-
-                    if(lastLevelHeights != null && lastLevelWidths != null)
-                    {
-                        for(int i =0; i < lastVerticalLevels; ++i)
-                        {
-                            lastSize += lastLevelWidths[i] * lastLevelHeights[i];
-                        }
-                    }
-
-					string[] oldList = new string[lastSize];
-                    if (listGridSelStrings != null)
-                    {
-                        Array.Copy(listGridSelStrings, oldList, (oldList.Length > listGridSelStrings.Length) ? listGridSelStrings.Length : oldList.Length);
-                    }
-                    
-					listGridSelStrings = new string[totalSize];
-				    Array.Copy(oldList, listGridSelStrings, (oldList.Length > listGridSelStrings.Length) ? listGridSelStrings.Length : oldList.Length);
-				}
-
                 height = currentLevelHeights[levelSelection];
 				width = currentLevelWidths[levelSelection];
 				lastHeight = lastLevelHeights[levelSelection];
 				lastWidth = lastLevelWidths[levelSelection];
-				int lastLength = lastHeight * lastWidth;
-				int lastStart = levelSelection * lastLength;
-				int currentStart = levelSelection * height * width;
 
+				// if the size has changed copy all the data and display the new grid
                 if (lastHeight != height || lastWidth != width || lastLevelSelection != levelSelection)
                 {
-                    Array.Copy(currentLevelWidths, lastLevelWidths, lastLevelWidths.Length);
-                    Array.Copy(currentLevelHeights, lastLevelHeights, lastLevelHeights.Length);
-                    // We need to copy a section of a 3d array to an array
-                    string[] temp = new string[lastLength];
+					if (lastHeight < height)
+					{
+						// need to add rows
+						for (int i = lastHeight; i < height; ++i)
+						{
+							listGridSelStrings[levelSelection].Add(new List<string>());
+						}
+					}
+					else if (lastHeight > height)
+					{
+						// need to remove rows
+						listGridSelStrings[levelSelection].RemoveRange(height, lastHeight - height);
+					}
 
-                    // Copy from the start of the grid relative to the rest to the temp array
-                    Array.Copy(listGridSelStrings, lastStart, temp, 0, lastLength);
+					for (int i = 0; i < listGridSelStrings[levelSelection].Count; ++i)
+					{
+						// need to add columns
+						if (lastWidth < width || listGridSelStrings[levelSelection][i].Count < width)
+						{
+							int count = (width - lastWidth > width - listGridSelStrings[levelSelection][i].Count) ?
+								width - lastWidth : width - listGridSelStrings[levelSelection][i].Count;
+							for (int j = 0; j < count; ++j)
+							{
+								listGridSelStrings[levelSelection][i].Add(blockTypes[defaultBlockIndex]);
+							}
+						}
+						// need to remove columns
+						else if (lastWidth > width)
+						{
+							listGridSelStrings[levelSelection][i].RemoveRange(width, lastWidth - width);
+						}
+
+					}
+
+					// copy the currents to the last frames
+					Array.Copy(currentLevelWidths, lastLevelWidths, lastLevelWidths.Length);
+                    Array.Copy(currentLevelHeights, lastLevelHeights, lastLevelHeights.Length);
+
+					// create a new grid of strings for display
                     gridSelStrings = new string[height * width];
-                    Array.Copy(temp, gridSelStrings, (temp.Length > gridSelStrings.Length) ? gridSelStrings.Length : temp.Length);
                 }
 
+				// copy the respective contents to the grid for display
+				if(gridSelStrings != null)
+				{
+					for (int j = 0; j < height; ++j)
+					{
+						for (int k = 0; k < width; ++k)
+						{
+							gridSelStrings[width * j + k] = listGridSelStrings[levelSelection][j][k];
+						}
+					}
+				}
+
+				// if we have a grid display it and change the text to the current selection if need be
                 if (height > 0 && width > 0)
 				{
 					gridSel = GUILayout.SelectionGrid(gridSel, gridSelStrings, width);
+
+					if (toggleSelector && gridSelStrings.Length > 0)
+					{
+						gridSelStrings[gridSel] = blockTypes[blockSelectionIndex];
+					}
 				}
 				
-
-				if (toggleSelector && gridSelStrings.Length > 0)
+				// copy the changes back over
+				if (gridSelStrings != null && listGridSelStrings != null && listGridSelStrings.Count > 0)
 				{
-					gridSelStrings[gridSel] = blockTypes[blockSelection];
-				}
-
-				if (gridSelStrings != null && listGridSelStrings != null && listGridSelStrings.Length > 0)
-				{
-					Array.Copy(gridSelStrings, 0, listGridSelStrings, lastStart, gridSelStrings.Length);
+					for(int j = 0; j < height; ++j)
+					{
+						for(int k = 0; k < width; ++k)
+						{
+							listGridSelStrings[levelSelection][j][k] = gridSelStrings[width * j + k];
+						}
+					}
 				}
 			}
 
@@ -163,12 +224,41 @@ public class MapCreationTool : EditorWindow
 
 			if (GUILayout.Button("Create Level"))
 			{
-				GenerateMap(verticalLevels, listGridSelStrings, blockTypes, currentLevelHeights, currentLevelWidths, scriptableObjectName);
+				// need to copy listrgridselstrings into an array
+				int totalSize = 0;
+				for(int i = 0; i < verticalLevels; ++i)
+				{
+					totalSize += currentLevelHeights[i] * currentLevelWidths[i];
+				}
+
+				string[] tempString = new string[totalSize];
+				int index = 0;
+				for (int i = 0; i < listGridSelStrings.Count; ++i)
+				{
+					for(int j = 0; j < listGridSelStrings[i].Count; ++j)
+					{
+						for(int k = 0; k < listGridSelStrings[i][j].Count; ++k)
+						{
+							tempString[index] = listGridSelStrings[i][j][k];
+							index++;
+						}
+					}
+				}
+				GenerateMap(verticalLevels, tempString, blockTypes, currentLevelHeights, currentLevelWidths, scriptableObjectName);
 			}
 		}
-		// TO DO: Add the option to grab a ton of tiles with right click
-		// OR do it with numbers idk
-		//Debug.Log(Event.current.button);
+
+		if(Event.current.keyCode == KeyCode.Z && Event.current.type == EventType.KeyUp)
+		{
+			toggleSelector = !toggleSelector;
+			EditorWindow.focusedWindow.Repaint();
+		}
+		else if(Event.current.keyCode == KeyCode.X && Event.current.type == EventType.KeyUp)
+		{
+			ChangeAll(blockSelectionIndex);
+			EditorWindow.focusedWindow.Repaint();
+		}
+
 		lastVerticalLevels = verticalLevels;
 		lastLevelSelection = levelSelection;
 	}
@@ -229,9 +319,19 @@ public class MapCreationTool : EditorWindow
 		scriptableObject.blockTypes = blockTypes;
 		scriptableObject.levelHeights = levelHeights;
 		scriptableObject.levelWidths = levelWidths;
-		Debug.Log(verticalLevels);
 		EditorUtility.SetDirty(scriptableObject);
 		AssetDatabase.SaveAssets();
 		AssetDatabase.Refresh();
+	}
+
+	void ChangeAll(int selectionIndex)
+	{
+		for (int j = 0; j < listGridSelStrings[levelSelection].Count; ++j)
+		{
+			for (int k = 0; k < listGridSelStrings[levelSelection][j].Count; ++k)
+			{
+				listGridSelStrings[levelSelection][j][k] = blockTypes[selectionIndex];
+			}
+		}
 	}
 }
