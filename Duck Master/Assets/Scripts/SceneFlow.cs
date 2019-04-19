65595aa37929bf3d8b9622e94d8b176285cce677
Tom Lunday <thomas.lunday@mymail.champlain.edu>
2019-04-19 15:50:00 -0400

    Updated animation controller, Added main menu scene, Made journal entries save across scenes

---
+++ Duck Master/Assets/Scripts/SceneFlow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneFlow : MonoBehaviour
+{
+    Animator anim;
+    int setToLoad = 0;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void GoToScene(int SceneToLoad)
+    {
+        anim.SetBool("Block", true);
+        setToLoad = SceneToLoad;
+    }
+
+    public void ActualSceneLoad()
+    {
+        SceneManager.LoadSceneAsync(setToLoad);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
