11fe72ec57bb12929c525d22bfe04ccfbeca6ae7
Tom Lunday <thomas.lunday@mymail.champlain.edu>
2019-04-29 00:07:55 -0400

    Added sound system stuff and settings menu

--- Duck Master/Assets/Scripts/SettingsMenu.cs
+++ Duck Master/Assets/Scripts/SettingsMenu.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
+    [SerializeField]
+    AudioMixer PlayerSounds;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,10 +24,50 @@ public class SettingsMenu : MonoBehaviour
     public void Toggle()
     {
         gameObject.SetActive(!gameObject.activeInHierarchy);
+        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByBuildIndex(0))
+        {
+            GetComponentInChildren<Button>().image.enabled = false;
+        }
     }
 
     public void LoadMenu()
     {
         SceneManager.LoadScene(0);
+    }
+
+    public void UpdateVolume(string VolumeGroup, float newVolume)
+    {
+        switch (VolumeGroup)
+        {
+            case "Master":
+                PlayerSounds.SetFloat("MasterVol", newVolume);
+                break;
+            case "SFX":
+                PlayerSounds.SetFloat("SfxVol", newVolume);
+                PlayerSounds.SetFloat("PlayerVol", newVolume - 20);
+                break;
+            case "Music":
+                PlayerSounds.SetFloat("MusicVol", newVolume);
+                break;
+        }
+    }
+
+    public float GetVolume(string VolumeGroup)
+    {
+        float valueReturn = 0;
+
+        switch (VolumeGroup)
+        {
+            case "Master":
+                PlayerSounds.GetFloat("MasterVol", out valueReturn);
+                break;
+            case "SFX":
+                PlayerSounds.GetFloat("SfxVol", out valueReturn);
+                break;
+            case "Music":
+                PlayerSounds.GetFloat("MusicVol", out valueReturn);
+                break;
+        }
+        return valueReturn;
     }
 }
---
+++ Duck Master/Assets/Scripts/SoundStuff/SoundSlider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SoundSlider : Slider
+{
+    SettingsMenu sm;
+
+    protected override void Start()
+    {
+        transform.Find("Label").GetComponent<Text>().text = name;
+        base.Start();
+    }
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        sm = FindObjectOfType<SettingsMenu>();
+        value = sm.GetVolume(name);
+    }
+
+    public void UpdateSlider()
+    {
+        sm.UpdateVolume(name, value);
+    }
+}
